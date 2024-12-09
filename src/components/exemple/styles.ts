b55b6b51a74b82fae3fdefc7908b0afc3f0704e0
Amilcar Junior <amilcar.oliveira@elastic.fit>
2024-12-09 20:16:00 -0300

    added stylelint

--- src/components/exemple/styles.ts
+++ src/components/exemple/styles.ts
@@ -2,70 +2,71 @@ import styled, { css } from 'styled-components/native';
 import {View} from 'react-native';
 
 export const Container  = styled.View`
-    width: 90%;
     height: 60%;
+    width: 90%;
+    align-items: center;
+    padding: 10px;
     border-width: 2px;
     border-color: blueviolet;
-    align-items: center;
     border-radius: 20px;
 `;
 
 export const Container2 = styled(View)`
-    width: 90%;
     height: 60%;
+    width: 90%;
     border-width: 2px;
     border-color: blueviolet;
     border-radius: 20px;
 `;
 
 export const Retangulo = styled.View`
-    width: 90%;
     height: 50px;
-    margin-top: 30px;
-    background-color: blue;  
+    width: 90%;
+    background-color: blue;
+    margin-top: 30px;  
 `;
 
 export const Clone = styled(Retangulo)`
-    margin-left: 30px;
-    background-color: red;
     height: 30px;
+    background-color: red;
+    margin-left: 30px;
 `;
 export const CliqueMe2 = styled.TouchableOpacity.attrs(()=>({
     activeOpacity: 0,
 }))`
-    min-width:40%;
-    align-items: center;
     justify-content: center;
-    border-radius: 10px;
+    align-items: center;
     background-color: cyan;
-    margin-top: 20px;
     padding: 10px;
+    min-width:40%;
+    border-radius: 10px;
+    margin-top: 20px;
 `;
 export const CliqueMe1 = styled.TouchableOpacity.attrs(()=>{
     return {
         activeOpacity: 0.5
     }
 } )`
-    min-width:40%;
-    align-items: center;
     justify-content: center;
-    border-radius: 10px;
+    align-items: center;
     background-color: cyan;
-    margin-top: 20px;
     padding: 10px;
+    min-width:40%;
+    border-radius: 10px;
+    margin-top: 20px;
 `;
 export const CliqueMe = styled.TouchableOpacity.attrs(function(){
     return {
         activeOpacity: 0
     }
 })`
-    min-width:40%;
-    align-items: center;
     justify-content: center;
-    border-radius: 10px;
+    align-items: center;
     background-color: cyan;
-    margin-top: 20px;
     padding: 10px;
+    min-width:40%;
+    border-radius: 10px;
+    margin-top: 20px;
 `;
 
 const CommonProps = css`
@@ -83,11 +84,11 @@ export const FirstText = styled.Text`
 `;
 
 export const Circle = styled.View`
-    width: 100px;
     height: 100px;
+    width: 100px;
+    background-color: yellow;
     border-radius: 100px;
     margin-top: 20px;
-    background-color: yellow;
 `;
 
 
