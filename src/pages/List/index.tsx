75848ea65881cbb7c5d75ce75271f07330161894
Amilcar Junior <amilcar.oliveira@elastic.fit>
2025-01-26 14:46:59 -0300

    added gradient in page

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {View, Text} from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
+import { ThemeContext } from 'styled-components/native';
+
 const List: React.FC = ()=> {
+    const Theme = useContext(ThemeContext);
+
+    const colors = [
+        Theme?.background!,
+        Theme?.background!,
+        Theme?.background!,
+        Theme?.background!,
+        Theme?.background!,
+        Theme?.background!,
+        Theme?.background!,
+        Theme?.background! + 90
+    ]
+
     return (
-        <View> 
+        <LinearGradient
+            style={{flex:1}}
+            colors={colors}
+            start={{x:-1 ,y: -1.5 }}
+            end={{x: 1, y: 1 }}
+        > 
             <Text> Page List</Text>
-        </View>
+        </LinearGradient>
     )
 };
 
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import {View, Text, FlatList} from 'react-native';
 import { ContainerButtons, ContainerProfile, ContainerSkills, HeaderInfo, HeaderText, TextGeneric, TitleSkillContent } from './styles';
 import SnippetComponent from '../../components/userSnippet';
@@ -6,7 +6,12 @@ import ButtomComponent from '../../components/Buttom';
 import SwippeComponent from '../../components/Swippe';
 import MedalSkillComponent from '../../components/MedallSkills';
 import { MedalSkillComponentProps } from '../../components/MedallSkills/Models';
+import LinearGradient from 'react-native-linear-gradient';
+import { ThemeContext } from 'styled-components/native';
+
 const Profile: React.FC = ()=> {
+
+    const Theme = useContext(ThemeContext);
 
     const [ actionActiveIndex, setActionActiveIndex] =  useState(0);
 
@@ -23,51 +28,55 @@ const Profile: React.FC = ()=> {
     ];
 
     return (
-        <ContainerProfile> 
-            <SnippetComponent/>
-            <HeaderInfo>
-                <HeaderText fs={50} bold wd={45}>
-                    React
-                    <HeaderText fs={35} bold wd={40}>Native</HeaderText>
-                </HeaderText>
-                <HeaderText fs={12} bold={false} wd={55}>
-                    React Native une o melhor dos dois mundos: o desempenho de aplicativos nativos e a flexibilidade do desenvolvimento em JavaScript onde é possível  criar experiências incríveis com uma única base de código, permitindo entregar soluções rápidas, eficientes e escaláveis.
-                </HeaderText>
-            </HeaderInfo>
-            <ContainerButtons>
-                <ButtomComponent icon='Email' label='E-mail'/>
-                <ButtomComponent icon='Linkedin' label='Linkedin'/>
-                <ButtomComponent icon='GitHub'/>
-                <ButtomComponent icon='GitLab'/>
-                <SwippeComponent label='Contate-me' width={230}/>
-                <ButtomComponent icon='Instagram'/>
-                <ButtomComponent icon='Facebook'/>
-            </ContainerButtons>
-            <ContainerSkills>
-                <TitleSkillContent> Skills </TitleSkillContent>
-                <FlatList 
-                    data={Medals}
-                    horizontal
-                    keyExtractor={(item)=> item.id}
-                    renderItem={({item, index})=> (
-                        <MedalSkillComponent 
-                            onAnimationEnd={()=> {setActionActiveIndex((prevState)=> (prevState + 1) )}}
-                            currentIndex={index === actionActiveIndex}
-                            icon={item?.icon as MedalSkillComponentProps['icon']} 
-                            label={item?.label} 
-                        />
-                    )}
-                />
-               
-                <TitleSkillContent> Sobre mim: </TitleSkillContent>
-                <TextGeneric bold> Nome completo: <TextGeneric bold={false}> Junior Oliveira </TextGeneric> </TextGeneric>
-                <TextGeneric bold> Endereço: <TextGeneric bold={false}> Rua dos desenvolvedor, n 20 </TextGeneric> </TextGeneric>
-                <TextGeneric bold> Formação : <TextGeneric bold={false}> Ciência Contábeis </TextGeneric> </TextGeneric>
-                <TextGeneric bold={false}> 
-                    Sou um desenvolvedor mobile com expertise em React Native, apaixonado por transformar ideias em aplicativos funcionais e intuitivos. Com uma sólida formação em ciência da computação. 
-                </TextGeneric>
-            </ContainerSkills>
-        </ContainerProfile>
+        <LinearGradient
+            style={{flex:1}}
+            colors={[Theme?.background!,Theme?.background!, Theme?.surface!]}
+        >
+                <SnippetComponent/>
+                <HeaderInfo>
+                    <HeaderText fs={50} bold wd={45}>
+                        React
+                        <HeaderText fs={35} bold wd={40}>Native</HeaderText>
+                    </HeaderText>
+                    <HeaderText fs={12} bold={false} wd={55}>
+                        React Native une o melhor dos dois mundos: o desempenho de aplicativos nativos e a flexibilidade do desenvolvimento em JavaScript onde é possível  criar experiências incríveis com uma única base de código, permitindo entregar soluções rápidas, eficientes e escaláveis.
+                    </HeaderText>
+                </HeaderInfo>
+                <ContainerButtons>
+                    <ButtomComponent icon='Email' label='E-mail'/>
+                    <ButtomComponent icon='Linkedin' label='Linkedin'/>
+                    <ButtomComponent icon='GitHub'/>
+                    <ButtomComponent icon='GitLab'/>
+                    <SwippeComponent label='Contate-me' width={230}/>
+                    <ButtomComponent icon='Instagram'/>
+                    <ButtomComponent icon='Facebook'/>
+                </ContainerButtons>
+                <ContainerSkills>
+                    <TitleSkillContent> Skills </TitleSkillContent>
+                    <FlatList 
+                        data={Medals}
+                        horizontal
+                        keyExtractor={(item)=> item.id}
+                        renderItem={({item, index})=> (
+                            <MedalSkillComponent 
+                                onAnimationEnd={()=> {setActionActiveIndex((prevState)=> (prevState + 1) )}}
+                                currentIndex={index === actionActiveIndex}
+                                icon={item?.icon as MedalSkillComponentProps['icon']} 
+                                label={item?.label} 
+                            />
+                        )}
+                    />
+                
+                    <TitleSkillContent> Sobre mim: </TitleSkillContent>
+                    <TextGeneric bold> Nome completo: <TextGeneric bold={false}> Junior Oliveira </TextGeneric> </TextGeneric>
+                    <TextGeneric bold> Endereço: <TextGeneric bold={false}> Rua dos desenvolvedor, n 20 </TextGeneric> </TextGeneric>
+                    <TextGeneric bold> Formação : <TextGeneric bold={false}> Ciência Contábeis </TextGeneric> </TextGeneric>
+                    <TextGeneric bold={false}> 
+                        Sou um desenvolvedor mobile com expertise em React Native, apaixonado por transformar ideias em aplicativos funcionais e intuitivos. Com uma sólida formação em ciência da computação. 
+                    </TextGeneric>
+                </ContainerSkills>
+
+        </LinearGradient>
     )
 };
 
