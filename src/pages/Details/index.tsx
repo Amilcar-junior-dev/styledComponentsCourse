1b37f1d92a7aee45e11007c243ff4f1ea7e5a747
Amilcar Junior <amilcar.oliveira@elastic.fit>
2025-01-31 10:31:57 -0300

    finish portfólio

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,34 +1,63 @@
-import React, { useContext } from 'react';
-import {View, Text, TouchableOpacity, ImageSourcePropType} from 'react-native';
-import { ContainerDetails, ContainerImageCard, HeaderImage } from './styles';
+import React, { ReactElement, useContext, useState } from 'react';
+import {View, Text, TouchableOpacity, ImageSourcePropType, Modal} from 'react-native';
+import { CardDescriptionTecnology, ContainerDetails, ContainerImageCard, ContainerInformation, ContainerTitleModal, Description, HeaderImage, ModalContainer, ModalContent, TitleDetails, TitleModal } from './styles';
 import { ThemeContext } from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 import Arrow from '../../assets/icons/arrow.svg'
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { CardComponentProps } from '../../components/Cards/Models';
+import ButtomComponent from '../../components/Buttom';
+import { ButtomComponentProps } from '../../components/Buttom/Models';
+
+import Email from '../../assets/icons/Email.svg';
+import Linkedin from '../../assets/icons/Linkedin.svg';
+import GitHub from '../../assets/icons/GitHub.svg';
+import Instagram from '../../assets/icons/Instagran.svg';
+import Facebook from '../../assets/icons/Facebook.svg';
+import ReactNative from '../../assets/icons/ReactLogo.svg';
+import JavaScript from '../../assets/icons/JavaScriptLogo.svg';
+import StyledComponents from '../../assets/icons/StyledComponentsLogo.svg';
+import Tailwind from '../../assets/icons/TailwindLogo.svg';
+import Firebase from '../../assets/icons/FirebaseLogo.svg';
+import HTML from '../../assets/icons/HTMLogo.svg';
+import CSS from '../../assets/icons/CSSLogo.svg';
+import TypeScript from '../../assets/icons/TypeScriptLogo.svg';
+import GitLab from '../../assets/icons/GitLab.svg';
 
 interface RouteProps {
     key: string;
     name: string;
     params: CardComponentProps;
-}
+};
+type keyIcon = {
+    [key: string]: ReactElement
+};
 
 const Details: React.FC = ()=> {
 
     const Theme = useContext(ThemeContext);
     const Route = useRoute<RouteProps>();
-    const Navigation = useNavigation()
+    const Navigation = useNavigation();
+
+    const [visible, setVisible] = useState({visible: false, icon: '', description: ''})
+    const colors  = Array(8).fill(Theme?.background).concat(Theme?.background! + 99)
     
-    const colors = [
-        Theme?.background!,
-        Theme?.background!,
-        Theme?.background!,
-        Theme?.background!,
-        Theme?.background!,
-        Theme?.background!,
-        Theme?.background!,
-        Theme?.background! + 90
-    ]
+    const IconSelected: keyIcon = {
+        Email: <Email width={35} height={35} color={Theme?.icon_primary}/>,
+        Linkedin: <Linkedin width={35} height={35} color={Theme?.icon_primary}/>,
+        GitHub: <GitHub width={35} height={35} color={Theme?.icon_primary}/>,
+        Instagram: <Instagram width={35} height={35} color={Theme?.icon_primary}/>,
+        Facebook: <Facebook width={35} height={35} color={Theme?.icon_primary}/>,
+        ReactNative: <ReactNative width={35} height={35} color={Theme?.icon_primary}/>,
+        JavaScript: <JavaScript width={35} height={35} color={Theme?.icon_primary}/>,
+        StyledComponents: <StyledComponents width={35} height={35} color={Theme?.icon_primary}/>,
+        Tailwind: <Tailwind width={35} height={35} color={Theme?.icon_primary}/>,
+        Firebase: <Firebase width={35} height={35} color={Theme?.icon_primary}/>,
+        HTML: <HTML width={35} height={35} color={Theme?.icon_primary}/>,
+        CSS: <CSS width={35} height={35} color={Theme?.icon_primary}/>,
+        TypeScript: <TypeScript width={35} height={35} color={Theme?.icon_primary}/>,
+        GitLab: <GitLab width={35} height={35} color={Theme?.icon_primary}/>,
+    }
 
     return (
         <ContainerDetails> 
@@ -47,6 +76,52 @@ const Details: React.FC = ()=> {
                 <HeaderImage>
                     <ContainerImageCard source={Route?.params?.item?.image}/>
                 </HeaderImage>
+                <ContainerInformation>
+                    <TitleDetails>
+                        {Route?.params?.item?.title}
+                    </TitleDetails>
+                    <Description wd={100}>
+                        {Route?.params?.item?.description}
+                    </Description>
+                    <TitleDetails>
+                       Tecnologias
+                    </TitleDetails>
+                    {
+                        Route?.params?.item?.technologies?.map((item, index)=>(
+                            <CardDescriptionTecnology 
+                                key={index}
+                                onPress={()=> setVisible({visible: true, icon: item?.icon, description: item?.description })}
+                            >
+                                <ButtomComponent 
+                                    icon={item?.icon as ButtomComponentProps['icon']} 
+                                    iconSize={35}
+                                />
+                                <Description wd={80}>
+                                    {item?.description}
+                                </Description>
+                            </CardDescriptionTecnology>
+                        ))
+                    }
+                </ContainerInformation>
+                <Modal
+                    transparent
+                    visible={visible.visible}
+                    animationType='slide'
+                    onDismiss={()=>{}}
+                    onRequestClose={()=>{}}
+                >
+                    <ModalContainer onPress={()=> setVisible({visible: false, icon: '', description: ''})}>
+                        <ModalContent>
+                            <ContainerTitleModal>
+                                {IconSelected[visible?.icon]}
+                                <TitleModal>{visible.icon}</TitleModal>
+                            </ContainerTitleModal>
+                            <Description wd={100} color={Theme?.icon_primary}> 
+                                {visible?.description}
+                            </Description>
+                        </ModalContent>
+                    </ModalContainer>
+                </Modal>
             </LinearGradient>
         </ContainerDetails>
     )
--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -19,3 +19,54 @@ export const ContainerImageCard = styled.Image`
     border-bottom-left-radius: 20px;
     border-bottom-right-radius: 20px;
 `;
+
+export const ContainerInformation = styled.ScrollView`
+    flex: 1;
+    padding: 10px;
+`;
+export const TitleDetails = styled.Text`
+    color: ${({theme})=> theme?.surface};
+    font-size: 25px;
+    font-weight: bold;
+    margin-top: 10px;
+    margin-bottom:10px;
+`;
+
+export const Description = styled(TitleDetails)<{wd: number, color?:string}>`
+    width: ${({wd})=> `${wd}%`};
+    color: ${({color, theme})=> color ? color : theme?.surface};
+    font-size:15px;
+    margin-top: 8px;
+`;
+export const CardDescriptionTecnology = styled.TouchableOpacity`
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+`;
+export const ModalContainer = styled.TouchableOpacity.attrs(()=>({activeOpacity:1}))`
+    flex:1;
+    align-items: center;
+    justify-content: center;
+`;
+export const ModalContent = styled.View`
+    width: 80%;
+    background-color: ${({theme})=> theme.surface};
+    padding: 10px;
+    border-radius: 15px;
+`;
+
+export const ContainerTitleModal = styled.View`
+    width: 100%;
+    align-items: center;
+    flex-direction: row;
+    margin-bottom:10px;
+`;
+
+export const TitleModal = styled.Text`
+    color: ${({theme})=> theme.icon_primary};
+    font-size: 25px;
+    font-weight: bold;
+    margin-left: 5px;
+`;
+
